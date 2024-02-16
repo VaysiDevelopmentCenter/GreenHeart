1d3b997bc20b68091a85023ccaea13d7de64125f
Vaysi Development Center <151166631+VaysiDevelopmentCenter@users.noreply.github.com>
2024-02-16 21:20:05 +0330

    Update generic.c

--- PolarisOS Redberry 0.9.18/kernel/generic.c
+++ PolarisOS Redberry 0.9.18/kernel/generic.c
@@ -10,9 +10,8 @@
  * early filesystems, and is ready to yield control to init.
  *
  * @copyright
- * This file is part of ToaruOS and is released under the terms
- * of the NCSA / University of Illinois License - see LICENSE.md
- * Copyright (C) 2021 K. Lange
+ * This file is part of PolarisOS and is released under the terms
+ * Copyright (C) VDC.
  */
 #include <kernel/generic.h>
 #include <kernel/args.h>
