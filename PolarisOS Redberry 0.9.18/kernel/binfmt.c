16a7cb173d80a45a2b6e865c6e9523f3c7c05f3a
Vaysi Development Center <151166631+VaysiDevelopmentCenter@users.noreply.github.com>
2024-02-16 21:18:59 +0330

    Update binfmt.c

--- PolarisOS Redberry 0.9.18/kernel/binfmt.c
+++ PolarisOS Redberry 0.9.18/kernel/binfmt.c
@@ -3,9 +3,8 @@
  * @brief Top-level executable parsing.
  *
  * @copyright
- * This file is part of ToaruOS and is released under the terms
- * of the NCSA / University of Illinois License - see LICENSE.md
- * Copyright (C) 2021 K. Lange
+ * This file is part of PolarisOS and is released under the terms
+ * Copyright (C) VDC.
  */
 #include <errno.h>
 #include <kernel/vfs.h>
